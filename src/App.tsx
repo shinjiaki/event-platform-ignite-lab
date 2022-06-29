281ba70aa5b882cb1144b85c66332c1eb6540920
shinjiaki <julio_shinji@hotmail.com>
2022-06-29 12:26:57 -0300

    aula 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { Sidebar } from "./components/Sidebar"
-import { Video } from "./components/Video"
-import { Event } from "./pages/Event"
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
+import { client } from "./lib/apollo"
+import { Router } from "./Router"
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Event />
-      <main className="flex flex-1">
-        <Video />
-        <Sidebar />
-
-      </main>
-    </div>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
